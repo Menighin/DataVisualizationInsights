02cbc97433d6f89bfd7e8ddafc2f1072717c9f89
Menighin <joao.menighin@gmail.com>
2016-07-26 23:00:17 -0300

    Initial commit with architecture draft

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    console.log('oi');
+    console.log(db.User);
+    console.log(User);
+});
+
+module.exports = router;
