e8102daf91cff276a5567eb1673a4aa000866f5d
João Menighin <joao.menighin@accenture.com>
2016-08-28 18:21:39 -0300

    Pan, zoom and tooltip

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,68 @@
 
-var h = 800;
-var svg = d3.select('.map')
-    .append('svg')
-    .attr('width', 'auto')
-    .attr('height', h);
+$(document).ready(function() {
 
-var projection = d3.geo.mercator()
-    .scale(1000)
-    .translate([1700, 120]);
+    var h = 800, 
+        w = $('.map').width(), 
+        initialScale = 1100;
 
-//Define path generator
-var path = d3.geo.path().projection(projection);
+    var svg = d3.select('.map')
+        .append('svg')
+        .attr('width', w)
+        .attr('height', h);
 
-d3.json('geo/mesorregiao.json', function(json) {
+    var g = svg.append('g').attr('transform', 'translate(0,0) scale(1)');
 
-    svg.selectAll('path')
-        .data(json.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .attr('stroke','#666')
-        .attr('stroke-width', 1)
-        .attr('fill', 'rgba(0,0,0,0)')
+    var projection = d3.geo.mercator()
+        .translate([1700, 110])
+        .scale(initialScale);
+
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 8])
+        .on("zoom", zoomed);
+
+    var tooltip = d3.select('.map').append('div')
+            .attr('class', 'hidden tooltip');
+
+    //Define path generator
+    var path = d3.geo.path().projection(projection);
+
+    svg.call(zoom).call(zoom.event);
+
+    d3.json('geo/mesorregiao.json', function(json) {
+
+        g.selectAll('path')
+            .data(json.features)
+            .enter()
+            .append('path')
+            .attr({
+                d: path,
+                stroke: '#666',
+                class: 'region'
+            }).on('mousemove', function(d) {
+                var mouse = d3.mouse(svg.node()).map(function(d) {
+                    return parseInt(d);
+                });
+                tooltip.classed('hidden', false)
+                    .attr('style', 'left:' + (mouse[0] + 15) + 'px; top:' + (mouse[1] + 25) + 'px')
+                    .html(d.properties.MESO.capitalize(true));
+            }).on('mouseout', function() {
+                tooltip.classed('hidden', true);
+            });
+    });
+
+    function zoomed() {
+        var strokeWidth = -0.1242*d3.event.scale + 1.1242;
+        g.attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+        g.selectAll('path').attr('stroke-width', strokeWidth);
+    }
 
 });
+
+String.prototype.capitalize=function(all){
+    var str = this.toLowerCase();
+    if(all){
+       return str.split(' ').map(function(e){return e.capitalize();}).join(' ');
+    }else{
+         return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+}
