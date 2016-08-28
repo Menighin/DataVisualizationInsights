68f62fdaae577d328db270a0263bab03bb6b728a
João Menighin <joao.menighin@accenture.com>
2016-08-28 04:31:25 -0300

    Displaying map and initial layout

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,26 @@
-d3.select("body").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
+
+var h = 800;
+var svg = d3.select('.map')
+    .append('svg')
+    .attr('width', 'auto')
+    .attr('height', h);
+
+var projection = d3.geo.mercator()
+    .scale(1000)
+    .translate([1700, 120]);
+
+//Define path generator
+var path = d3.geo.path().projection(projection);
+
+d3.json('geo/mesorregiao.json', function(json) {
+
+    svg.selectAll('path')
+        .data(json.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('stroke','#666')
+        .attr('stroke-width', 1)
+        .attr('fill', 'rgba(0,0,0,0)')
+
+});
